16b1cdef2b007e7d38f98bf98dfc85663bba7336
Mark P. <markpikaro@yandex.com>
2025-09-08 13:57:18 +0300

    Implement Player class and game state management

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,49 @@
 import { Server } from "socket.io";
 import dotenv from "dotenv";
+import { v4 as uuidv4 } from "uuid";
 
 dotenv.config({ path: ".env" });
 const VERSION = process.env.APP_VERSION;
+
+let colors = ["red", "blue", "green", "orange", "purple", "yellow", "gray"];
+
+class Player {
+  UUID;
+  socketID;
+  nickname;
+  color;
+  UUIDvalidUntil;
+
+  balance;
+  loans;
+  paintings;
+
+  constructor(UUID, socketID, nickname, UUIDvalidUntil) {
+    this.UUID = UUID;
+    this.socketID = socketID;
+    this.nickname = nickname;
+    this.color = colors.slice(Math.floor(Math.random() * colors.length), 1);
+    this.UUIDvalidUntil = UUIDvalidUntil;
+
+    this.balance = 3000;
+    this.loans = 0;
+    this.paintings = 0;
+  }
+
+  getPlayerInfo() {
+    console.log(`
+UUID: ${this.UUID}
+SocketID: ${this.socketID}
+Nickname: ${this.nickname}
+Color: ${this.color}
+Balance: ${this.balance}
+Loans: ${this.loans}
+Paintings: ${this.paintings}
+    `);
+  }
+}
+
+let players = [];
 
 const io = new Server(3001, {
   cors: {
